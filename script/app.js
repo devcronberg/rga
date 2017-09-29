ba9f1681ca3e5f1eebf97797a8d0703c7ae62950
unknown <admin@TA-8212-02.kursdom.net>
2017-09-29 14:33:21 +0200

    Initial checkin

---
+++ script/app.js
@@ -0,0 +1,5 @@
+document.getElementById("minP").innerHTML = "Dette kommer fra js " + new Date();
+
+document.getElementById("i1").onclick = function(){
+    document.getElementById("i1").style.color = "green";
+};
